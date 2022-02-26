e9db4b8f22e5fafc9fcc47a595045004d4c6bdca
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-26 16:09:52 -0500

    Configure Comment migration and link to user/author

--- db/migrate/20220226204559_create_comments.rb
+++ db/migrate/20220226204559_create_comments.rb
@@ -2,8 +2,15 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
+      t.integer :author_id
+      t.integer :post_id
+      t.text :text
 
       t.timestamps
     end
+    add_index :likes, :author_id
+    add_index :likes, :post_id
+    add_foreign_key :likes, :users
+    add_foreign_key :likes, :posts
   end
 end
