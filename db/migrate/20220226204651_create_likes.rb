773e2cfcc04f1e4e7a270c84a0902ce4f16036b7
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-26 16:08:36 -0500

    Configure Like migration and link to user/author

--- db/migrate/20220226204651_create_likes.rb
+++ db/migrate/20220226204651_create_likes.rb
@@ -1,8 +1,15 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
+      t.integer :author_id
+      t.integer :post_id
+      t.text :text
 
       t.timestamps
     end
+    add_index :likes, :author_id
+    add_index :likes, :post_id
+    add_foreign_key :likes, :users
+    add_foreign_key :likes, :posts
   end
 end
