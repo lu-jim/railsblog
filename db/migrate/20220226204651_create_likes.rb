b0cd089f4cff4927bc1f279283161898295e8fd8
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-28 18:39:20 -0500

    Refactor like class references

--- db/migrate/20220226204651_create_likes.rb
+++ db/migrate/20220226204651_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :author_id, index: true, foreign_key: { to_table: :users }
-      t.references :post_id, index: true, foreign_key: { to_table: :posts }
+      t.references :author, index: true, foreign_key: true
+      t.references :post, index: true, foreign_key: true
       t.text :text
 
       t.timestamps
