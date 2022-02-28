1f41438a83c1152b97e060b7d1ab1b29e2c26ea0
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-28 17:38:42 -0500

    Refactor comment class references

--- db/migrate/20220226204559_create_comments.rb
+++ db/migrate/20220226204559_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author_id, index: true, foreign_key: { to_table: :users }
-      t.references :post_id, index: true, foreign_key: { to_table: :posts }
+      t.references :author, index: true, foreign_key: { to_table: :users }
+      t.references :post, index: true, foreign_key: { to_table: :posts }
       t.text :text
 
       t.timestamps
