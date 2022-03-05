574962e0128e3a50371e2236be02b5d81f6908b6
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-05 16:35:19 -0500

    Expand post controller to support creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.new(post_params)
+    @post.user = @user
+    if @post.save
+      redirect_to user_post_url({ user_id: @user.id, id: @post.id })
+      flash[:success] = 'Post created successfully'
+    else
+      render :new, flash: { error: 'Please make sure your post is valid' }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:comment).permit(:text)
+  end
 end
