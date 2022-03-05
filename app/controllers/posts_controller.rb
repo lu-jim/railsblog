268afbf6f6f6068bfce1208916d7d21cfdf91fed
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-05 17:33:14 -0500

    Add form input in post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 
