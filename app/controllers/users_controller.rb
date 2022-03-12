f40404f3d2c59a0cffdf3fe6e273d074cd0e2612
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-12 00:10:13 -0500

    See all posts from most recent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(author_id: params[:id])
+    @posts = Post.where(author_id: params[:id]).order(created_at: :desc)
     @all = params[:all] unless params[:all].nil?
   end
 end
