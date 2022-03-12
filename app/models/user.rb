d8e423653f572fd4936ea6cbd5c9395e6dc4748e
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-12 02:04:26 -0500

    Add validation to user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
 
+  validate :name, presence: true
+  validate :name, presence: true
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
