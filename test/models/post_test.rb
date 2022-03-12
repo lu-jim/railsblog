2f3cf16386350535a8b1ba2f99407ea1a9fa5878
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-12 03:41:48 -0500

    Add tests for post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save post without title' do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test 'should not save post with a long title' do
+    long_title = 'Hi' * 150
+    post = Post.new(title: long_title, text: 'Looong title')
+    assert_not post.save
+  end
+
+  test 'should throw an length error when trying a long title' do
+    long_title = 'Hi' * 150
+    post = Post.new(title: long_title, text: 'Looong title')
+    assert_raises('250 characters is the maximum allowed') { post.save.errors }
+  end
 end
