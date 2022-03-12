96d6c7a2df6b80380a5714f277fede2f6aa124d9
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-12 04:05:34 -0500

    Add tests to user

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -14,7 +14,8 @@ class PostTest < ActiveSupport::TestCase
 
   test 'should throw an length error when trying a long title' do
     long_title = 'Hi' * 150
-    post = Post.new(title: long_title, text: 'Looong title')
-    assert_raises('250 characters is the maximum allowed') { post.save.errors }
+    first_user = User.create(name: 'Tom', photo: '', bio: 'Teacher from Mexico.', post_counter: 0)
+    post = Post.new(author: first_user, title: long_title, text: 'Looong title', like_counter: 0, comment_counter: 0)
+    assert_not post.valid?
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save user without name' do
+    user = User.new
+    assert_not user.save
+  end
+
+  test 'should throw an missing item error when name is empty' do
+    user = User.create(photo: '', bio: 'Teacher from Mexico.', post_counter: 0)
+    assert_raises('name cannot be blank') { user.save.errors }
+  end
 end
