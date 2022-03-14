acbeb684e9130320c9335642ed8e7fb3b2acfad4
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-14 16:55:24 -0400

    Modify login route to match wireframe

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
