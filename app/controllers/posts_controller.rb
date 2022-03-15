0f87d73ec9ff023d00410900cdd62dac5f18a4d4
eapenzacharias <eapenzac@gmail.com>
2022-03-15 19:18:08 +0100

    created ability model and added routes for destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    # @post =Post.find(params[:id])
+    @post.destroy
+    format.html { redirect_to user_url(@author), flash: { success: 'Post deleted successfully.' } }
+  end
+
   private
 
   def set_post
