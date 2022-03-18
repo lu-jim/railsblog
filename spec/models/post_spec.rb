70ee35a2e08af946a054b98ca15fad1a04be9afa
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-18 12:05:05 -0400

    Update user_spec to include login and confirmation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   before :each do
     @user = User.create(name: 'Tom',
-                        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
                         email: 'tomrails@mailinator.com',
                         password: '123123',
                         password_confirmation: '123123',
