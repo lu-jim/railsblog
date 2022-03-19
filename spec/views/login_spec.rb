5d2b1574e7c91f3814f2d77c07c5b730f118d63d
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-19 01:17:42 -0400

    Configure email field test for login

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'Login page', type: :system do
-  before do
+RSpec.describe 'The login process', type: :system do
+  let(:user) do
     User.create(id: 1, name: 'Tom', email: 'tomrails@mailinator.com',
                 password: '123123', posts_counter: 0, confirmed_at: Time.now)
   end
+  before :each do
+    visit '/users/sign_in'
+  end
 
-  describe 'Visiting the login page' do
-    before :each do
-      visit '/users/sign_in'
-    end
+  it 'I can see the email field', js: true do
+    expect(page).to have_content('Email')
   end
 end
