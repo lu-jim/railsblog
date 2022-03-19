c317bf9ebaef2fd893eec854c3601a6b24e4824d
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-19 11:47:58 -0400

    Write tests for user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(author_id: params[:id]).order(created_at: :desc)
+    @posts = Post.includes([:author]).order(created_at: :desc)
     @all = params[:all] unless params[:all].nil?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,6 @@ class User < ApplicationRecord
   end
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+  posts.includes([:author]).order(created_at: :desc).limit(3)
   end
 end
