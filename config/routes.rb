2aa1874a3c95f521b7e7cc9a9b84c7c0ccb5be6f
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-20 16:04:20 -0400

    Update routing to incorporate api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:new, :create]
+  namespace :api do
+    resources :users, only: %i[index show] do
+      resources :posts, only: %i[index show new create destroy] do
+        resources :comments, only: %i[new create destroy]
+        resources :likes, only: %i[new create]
+      end
+    end
+  end
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new create destroy]
+      resources :likes, only: %i[new create]
     end
   end
   # Defines the root path route ("/")
