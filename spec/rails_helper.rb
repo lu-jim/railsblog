de4a298369a9f6bc757ce218dc0b6177db3bc468
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-03-20 17:05:26 -0400

    Prepare rails spec for factorybot and shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,6 +23,14 @@ require 'capybara/rspec'
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -58,6 +66,8 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
